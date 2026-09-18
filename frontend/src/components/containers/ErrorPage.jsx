a7b9aaf41551d496fb5e5ce7735b97a69b631566
elliotracinep <racinenmimie1@gmail.com>
2026-09-17 21:47:57 -0400

    page erreur début - audio - sans css

--- frontend/src/components/containers/ErrorPage.jsx
+++ frontend/src/components/containers/ErrorPage.jsx
@@ -1,12 +1,18 @@
+import "./ErrorPage.css"
+import audio from "../../assets/audio/fail1.mp3"
 
 const ErrorPage = () => {
+    const start = () => {
+        new Audio(audio).play();
+    }
     return (
         <>
-        <main>
-            <div className="error-card">
-                <h1>Une erreur s'est produite...</h1>
-            </div>
-        </main>
+            <main>
+                <div className="error-card">
+                    <h1>Une erreur s'est produite...</h1>
+                    <button onClick={start}>célébrer votre défaite</button>
+                </div>
+            </main>
         </>
     );
 }
